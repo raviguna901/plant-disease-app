8a2af064d125b8632d047e66c1bc2888a27ceb64
RAVI GUNA <raviguna@RAVIs-MacBook-Air.local>
2026-02-18 23:02:08 +0530

    railway deploy

--- app.py
+++ app.py
@@ -181,4 +181,5 @@ def download_report():
 # ---------------- RUN ----------------
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
